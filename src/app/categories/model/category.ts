5cd5d0ecc74ef3e908a61465a22a1e6717f3c361
Camelia K <camelia.assobhei@gmail.com>
2024-03-07 17:03:33 +0100

    model category + get categories and id

--- src/app/categories/model/category.ts
+++ src/app/categories/model/category.ts
@@ -1,2 +1,19 @@
 export class Category {
+  public get name(): string {
+    return this._name;
+  }
+  public set name(value: string) {
+    this._name = value;
+  }
+  public get id(): number {
+    return this._id;
+  }
+  public set id(value: number) {
+    this._id = value;
+  }
+  constructor(
+    private _id: number,
+    private _name: string
+    // has many products
+  ){}
 }
