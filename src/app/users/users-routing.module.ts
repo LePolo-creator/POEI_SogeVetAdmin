b51c4686f6e9eebcf8beef8e12b5eeeb47131f48
Camelia K <camelia.assobhei@gmail.com>
2024-03-10 19:40:33 +0100

    front

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListUsersComponent } from './list-users/list-users.component';
-import { EditCategoryComponent } from '../categories/edit-category/edit-category.component';
+import { EditUserComponent } from './edit-user/edit-user.component';
 
 const routes: Routes = [
   {path:"", component:ListUsersComponent, pathMatch:"full"},
-  {path:"edit/:id", component:EditCategoryComponent},
+  {path:"edit/:id", component:EditUserComponent},
 ];
 
 @NgModule({
